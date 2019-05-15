080b6742947c12c27e061dc2317c3f889901c165
mixingpot <50566004+mixingpot@users.noreply.github.com>
2019-05-15 11:42:35 -0400

    Update main.R

--- main.R
+++ main.R
@@ -19,11 +19,11 @@ survey.summary <- data.frame(
   Lived_in_Ivy        = ifelse(!is.na(survey$Lived_in_Ivy) & grepl("Any", survey$Lived_in_Ivy), 1, 0),
   Q2_indirect         = ifelse(survey$q2 == "Yes - but only in media (TV, books, magazines, etc.) or when learning or discussing history", 1, 0),
   Q2_direct           = ifelse(grepl("Yes (", survey$q2, fixed=T), 1, 0),
-  Q3_both             = ifelse(survey$q3 == "Yes - both", 1, 0),
-  Q4_biz              = ifelse(survey$q4 == "Business", 1, 0),
-  Q4_gov              = ifelse(survey$q4 == "Government", 1, 0),
-  Q4_edu              = ifelse(survey$q4 == "Higher Education", 1, 0),
-  Q4_NA               = ifelse(survey$q4 == "N/A or I don't know", 1, 0),
+  Q3_both             = ifelse((!is.na(survey$q3)) & survey$q3 == "Yes - both", 1, 0),
+  Q4_biz              = ifelse((!is.na(survey$q4)) & survey$q4 == "Business", 1, 0),
+  Q4_gov              = ifelse((!is.na(survey$q4)) & survey$q4 == "Government", 1, 0),
+  Q4_edu              = ifelse((!is.na(survey$q4)) & survey$q4 == "Higher Education", 1, 0),
+  Q4_NA               = ifelse((!is.na(survey$q4)) & survey$q4 == "N/A or I don't know", 1, 0),
   Current_Region_East_North_Central = ifelse(!is.na(survey$Region) & survey$Region == "East North Central",1,0),
   Current_Region_East_South_Central = ifelse(!is.na(survey$Region) & survey$Region == "East South Central",1,0),
   Current_Region_Middel_Atlantic = ifelse(!is.na(survey$Region) & survey$Region    == "Middle Atlantic",1,0),
@@ -37,6 +37,7 @@ survey.summary <- data.frame(
   Gender_Male = ifelse(survey$Gender=="Male",1 , 0)
 )
 
+sum(is.na(survey.summary)) # should be 0
 
 stargazer(survey.summary, 'html')
 
