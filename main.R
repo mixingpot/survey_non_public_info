5ff3e817ad6f19e54b80c5937c66abd1b8295f28
mixingpot <50566004+mixingpot@users.noreply.github.com>
2019-05-15 09:24:27 -0400

    Update main.R

--- main.R
+++ main.R
@@ -3,31 +3,44 @@
 # (c) 2019, MixingPot.ai
 
 # Libraries + Options -----------------------------------------------------
-if(!require(pacman)); install.packages("pacman"); require(pacman)
+require(pacman)
 p_load(tidyverse, sjPlot, stargazer, survey)
 
+if(!require(gt)) remotes::install_github("rstudio/gt"); require(gt)
+
 # Raw Data ----------------------------------------------------------------
-survey <- read_csv("survey1_clean.csv") # deleted junk and renamed cols in a spreadsheet
-saveRDS(survey, "survey.RDS")
+survey <- read_csv("C://users//jmiller//Desktop//surv//survey1_clean.csv")
+saveRDS(survey, "C://users//jmiller//Desktop//surv//survey.RDS")
+
 
 # Feature Engineering -----------------------------------------------------
 survey.summary <- data.frame(
-  Lived_in_Atlanta    = ifelse(!is.na(survey$Lived_in_Atlanta) & survey$Lived_in_Atlanta   == "Atlanta", 1, 0),
+  Lived_in_Atlanta    = ifelse(!is.na(survey$Lived_in_Atlanta) & survey$Lived_in_Atlanta     == "Atlanta", 1, 0),
   Lived_in_Baltimore  = ifelse(!is.na(survey$Lived_in_Baltimore) & survey$Lived_in_Baltimore == "Baltimore", 1, 0),
-  Lived_in_SF         = ifelse(!is.na(survey$Lived_in_SF) & survey$Lived_in_SF        == "SF", 1, 0),
+  Lived_in_SF         = ifelse(!is.na(survey$Lived_in_SF) & survey$Lived_in_SF               == "SF", 1, 0),
   Lived_in_Ivy        = ifelse(!is.na(survey$Lived_in_Ivy) & grepl("Any", survey$Lived_in_Ivy), 1, 0),
   Q2_indirect         = ifelse(survey$q2 == "Yes - but only in media (TV, books, magazines, etc.) or when learning or discussing history", 1, 0),
-  Q2_direct           = ifelse(survey$q2 %in% c("Yes (multiple)", "Yes (please descrie"), 1, 0),
+  Q2_direct           = ifelse(grepl("Yes (", survey$q2, fixed=T), 1, 0),
   Q3_both             = ifelse(survey$q3 == "Yes - both", 1, 0),
   Q4_biz              = ifelse(survey$q4 == "Business", 1, 0),
   Q4_gov              = ifelse(survey$q4 == "Government", 1, 0),
   Q4_edu              = ifelse(survey$q4 == "Higher Education", 1, 0),
-  Q4_NA               = ifelse(survey$q4 == "N/A or I don't know", 1, 0)
+  Q4_NA               = ifelse(survey$q4 == "N/A or I don't know", 1, 0),
+  Current_Region_East_North_Central = ifelse(survey$Region == "East North Central",1,0),
+  Current_Region_East_South_Central = ifelse(survey$Region == "East South Central",1,0),
+  Current_Region_Middel_Atlantic = ifelse(survey$Region    == "Middle Atlantic",1,0),
+  Current_Region_Mountain = ifelse(survey$Region           == "Mountain",1,0),
+  Current_Region_New_England = ifelse(survey$Region        == "New England",1,0),
+  Current_Region_Pacific = ifelse(survey$Region            == "Pacific",1,0),
+  Current_Region_South_Atlantic = ifelse(survey$Region     == "South Atlantic",1,0),
+  Current_Region_West_North_Central = ifelse(survey$Region == "West North Central",1,0),
+  Current_Region_West_South = ifelse(survey$Region         == "West South Central",1,0),
+  Current_Region_Pacific = ifelse(survey$Region            == "Pacific",1,0)
 )
 
 stargazer(survey.summary, 'html')
 
-saveRDS(survey.summary, "survey.summary.RDS")
+saveRDS(survey.summary, "C://users//jmiller//Desktop//surv//survey.summary.RDS") # write out (for .Rmd)
 
 
 # EDA ---------------------------------------------------------------------
@@ -81,16 +94,17 @@ prop.table(table(survey$q3))
 table(survey$q4)
 prop.table(table(survey$q4))
 
-# Sample Weighting --------------------------------------------------------
-# TO DO!
 
-# data = cbind(preYear, income, gender, ethnicity, postYear)
-# data = as.data.frame(data)
-# 
-# 
-# data.svy.unweighted <- svydesign(ids=~1, data=data)
-#                      
-# data.svy.rake.trim <- trimWeights(data.svy.rake, lower=0.3, upper=3,
-#                                   strict=TRUE)
-# 
-# svymean(data, data.svy.rake.trim)
+# Sample Weighting --------------------------------------------------------
+# Working Area (not complete)
+
+library(survey)
+
+data.svy.unweighted <- svydesign(ids=~1, data=survey)
+data.svy.rake.trim  <- trimWeights(survey, lower=0.3, upper=3,
+                                  strict=TRUE)
+
+svymean(data, data.svy.rake.trim)
+
+
+
